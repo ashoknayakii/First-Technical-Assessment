e48b5cb5e5a86f48ad5f74146847c7410f26dd3d
Ashok Nayak <ashok.nayakii@gmail.com>
2022-01-21 01:24:07 -0500

    initial pseudocode and functions outlined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 // GIVEN I am taking a code quiz
 
-startEl = document.querySelector("#start-button")
+startEl = document.getElementbyId("#start-btn")
 // WHEN I click the Start button
 
-function startQuiz (event) {
-    var
-}
+var counter = 75;
+var isRunning = false;
+var startCountdown = setInterval(countdown, 1000)
 
+startEl.addEventListener("click", startQuiz)
+
+function startQuiz (event) {
+    
+    
+
+   
+}
 
 
 // THEN a timer starts and I am presented with a question
 
-
+function viewQuestion() {}
 
 // WHEN I answer a question
 
 
+function selectAnswer() {}
 
 
 // THEN I am presented with another question
@@ -23,13 +32,9 @@ function startQuiz (event) {
 
 
 // WHEN I answer a question incorrectly
-
-
-
-
 // THEN time is subtracted from the clock
 
-
+var questions = []
 
 // WHEN all questions are answered or the timer reaches 0
 
@@ -45,4 +50,3 @@ function startQuiz (event) {
 
 // Questions should be put into an array as well as answers?
 
-startEl.addEventListener("click", startQuiz);
