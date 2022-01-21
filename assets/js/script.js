05676617d1e34bba2b95aa3139b839f26724eb24
Ashok Nayak <ashok.nayakii@gmail.com>
2022-01-20 23:24:58 -0500

    set up initial files for coding quiz

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// GIVEN I am taking a code quiz
+
+startEl = document.querySelector("#start-button")
+// WHEN I click the Start button
+
+function startQuiz (event) {
+    var
+}
+
+
+
+// THEN a timer starts and I am presented with a question
+
+
+
+// WHEN I answer a question
+
+
+
+
+// THEN I am presented with another question
+
+
+
+// WHEN I answer a question incorrectly
+
+
+
+
+// THEN time is subtracted from the clock
+
+
+
+// WHEN all questions are answered or the timer reaches 0
+
+
+
+// THEN the game is over
+
+
+
+// WHEN the game is over
+
+// THEN I can save my initials and score
+
+// Questions should be put into an array as well as answers?
+
+startEl.addEventListener("click", startQuiz);
